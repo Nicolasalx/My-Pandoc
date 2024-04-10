2b309f81271098af46e6b6f0cb1afab2e7d712d7
Ulysse <ulysse.rousselet@epitech.eu>
2024-04-10 20:59:41 +0200

    add: Beginning of parsing Json

--- src/ParseJson/ParseJson.hs
+++ src/ParseJson/ParseJson.hs
@@ -7,6 +7,27 @@
 
 module ParseJson.ParseJson (parseJson) where
 import Content (PHeader(..), PBody(..))
+import Lib (parseString)
+
+parseEachLine :: [String] -> PHeader -> Either String PHeader
+parseEachLine [] x = Right x
+parseEachLine (x:xs) pHeader
+    | Just ("\"title\":", value) <- parseString "\"title\":" x =
+        parseEachLine xs pHeader { header_title = value }
+    | Just ("\"author\":", value) <- parseString "\"author\":" x =
+        parseEachLine xs pHeader { author = Just value }
+    | Just ("\"date\":", value) <- parseString "\"date\":" x =
+        parseEachLine xs pHeader { date = Just value }
+    | otherwise = Left "Error: Invalid header format or field"
+
+parseJsonHeader :: [String] -> IO (Either String PHeader)
+parseJsonHeader [] = return $ Left "Empty file"
+parseJsonHeader (x:xs) = return $ parseEachLine (x:xs) (PHeader { header_title = "", author = Nothing, date = Nothing })
 
 parseJson :: String -> IO (Either String (PHeader, PBody))
-parseJson file = return (Right ((PHeader "" (Just "") (Just "")), PBody []))
+parseJson file_content = do
+    let allLines = lines file_content
+    headerResult <- parseJsonHeader allLines
+    case headerResult of
+        Right pHeader -> return $ Right (pHeader, PBody [])
+        Left err -> return $ Left err
