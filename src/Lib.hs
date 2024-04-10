ff6804f5311e97acc1455868efc56b36f52af5fd
Guillaume <guillaume.dubuis@epitech.eu>
2024-04-10 13:52:45 +0200

    add: ex01 and ex02 boostrap mypandoc

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,20 @@ module Lib
     ( someFunc
     ) where
 
+
+type Parser a = String -> Maybe (a , String)
+
+parseChar :: Char -> Parser Char
+parseChar c (x:xs)
+  | x == c = Just (c, xs)
+parseChar _ _ = Nothing
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar [] _ = Nothing
+parseAnyChar _ [] = Nothing
+parseAnyChar (c:cs) (x:xs)
+  | x == c = Just (c, xs)
+  | otherwise = parseAnyChar cs (x:xs)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
