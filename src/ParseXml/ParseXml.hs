574ad4332f1fdf1c9bec93239ee7630cc5fa6363
Guillaume <guillaume.dubuis@epitech.eu>
2024-04-11 14:18:07 +0200

    add: get title of header in xml

--- src/ParseXml/ParseXml.hs
+++ src/ParseXml/ParseXml.hs
@@ -6,7 +6,29 @@
 -}
 
 module ParseXml.ParseXml (parseXml) where
+import Lib (parseString)
 import Content (PHeader(..), PBody(..))
+import Data.Maybe (fromMaybe)
 
 parseXml :: String -> IO (Either String (PHeader, PBody))
-parseXml file = return (Right ((PHeader "" (Just "") (Just "")), PBody []))
+parseXml file_content = do
+    let headerResult = do
+            title <- parseDocumentTitle file_content
+            return title
+    case headerResult of
+        Just header -> return $ Right (header, PBody [])
+        Nothing -> return $ Left "Erreur: thibaud pas content"
+
+parseDocumentTitle :: String -> Maybe PHeader
+parseDocumentTitle input = do
+    (_, rest1) <- parseString "<title>" input
+    (title, rest2) <- parseUntil '>' rest1
+    return $ PHeader title Nothing Nothing
+
+parseUntil :: Char -> String -> Maybe (String, String)
+parseUntil c "" = Nothing
+parseUntil c (x:xs)
+    | x == c = Just ("", xs)
+    | otherwise = do
+        (parsed, rest) <- parseUntil c xs
+        return (x:parsed, rest)
