eed1f7562bfd5e9f4914babee570b5e1bb2c7e0f
Ulysse Rousselet <ulysse.rousselet@epitech.eu>
2024-04-11 11:05:09 +0200

    add: parsing Json Header

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,11 @@
-module Lib (parseString) where
+module Lib (parseString, parseJsonKey) where
 
 type Parser a = String -> Maybe (a , String)
 
-parseChar :: Char -> Parser Char
-parseChar c (x:xs)
-    | x == c = Just (c, xs)
-parseChar _ _ = Nothing
+-- parseChar :: Char -> Parser Char
+-- parseChar c (x:xs)
+--     | x == c = Just (c, xs)
+-- parseChar _ _ = Nothing
 
 parseString :: String -> Parser String
 parseString [] input = Just ([], input)
@@ -14,31 +14,9 @@ parseString (s:str) (x:xs)
   | otherwise = Nothing
 parseString _ _ = Nothing
 
-parseAnyChar :: String -> Parser Char
-parseAnyChar [] _ = Nothing
-parseAnyChar _ [] = Nothing
-parseAnyChar (c:cs) (x:xs)
-    | x == c = Just (c, xs)
-    | otherwise = parseAnyChar cs (x:xs)
-
-parseOr :: Parser a -> Parser a -> Parser a
-parseOr p1 p2 s = (p1 s) `or` (p2 s)
-    where
-        or Nothing Nothing = Nothing
-        or (Just a) _ = Just a
-        or _ (Just b) = Just b
-
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd p1 p2 s = p1 s >>= \ (a, s') -> p2 s' >>= \ (b, s'') -> Just((a, b), s'')
-
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-parseAndWith f p1 p2 s = p1 s >>= \ (a, s') -> p2 s' >>= \ (b, s'') -> Just(f a b, s'')
-
-parseMany :: Parser a -> Parser [a]
-parseMany f s = f s >>= \ (a, s') -> parseMany f s' >>= \ (as, s'') -> Just(a:as, s'')
-
--- C'est pas sque vous croyez
 parseJsonKey :: String -> Int -> Parser String
+parseJsonKey [_] _ [] = Nothing
+parseJsonKey (_:_:_) _ [] = Nothing
 parseJsonKey [] state input | head input == ' ' = parseJsonKey [] state (tail input)
                             | head input == '\"' = parseJsonKey [] state (tail input)
                             | last input == ',' = parseJsonKey [] state (init input)
@@ -51,3 +29,28 @@ parseJsonKey (s:str) state (x:xs)
   | x == '\"' && s == '\"' && state == 3 = parseJsonKey str 2 (xs)
   | s == ':' && state == 2 = parseJsonKey str 1 (xs)
   | otherwise = Nothing
+
+-- lib bootstrap
+
+-- parseAnyChar :: String -> Parser Char
+-- parseAnyChar [] _ = Nothing
+-- parseAnyChar _ [] = Nothing
+-- parseAnyChar (c:cs) (x:xs)
+--     | x == c = Just (c, xs)
+--     | otherwise = parseAnyChar cs (x:xs)
+
+-- parseOr :: Parser a -> Parser a -> Parser a
+-- parseOr p1 p2 s = (p1 s) `or` (p2 s)
+--     where
+--         or Nothing Nothing = Nothing
+--         or (Just a) _ = Just a
+--         or _ (Just b) = Just b
+
+-- parseAnd :: Parser a -> Parser b -> Parser (a, b)
+-- parseAnd p1 p2 s = p1 s >>= \ (a, s') -> p2 s' >>= \ (b, s'') -> Just((a, b), s'')
+
+-- parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+-- parseAndWith f p1 p2 s = p1 s >>= \ (a, s') -> p2 s' >>= \ (b, s'') -> Just(f a b, s'')
+
+-- parseMany :: Parser a -> Parser [a]
+-- parseMany f s = f s >>= \ (a, s') -> parseMany f s' >>= \ (as, s'') -> Just(a:as, s'')
