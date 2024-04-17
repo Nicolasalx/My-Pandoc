eac570eeedfbc6aec65beecd6fb8f20de5f5fb2a
Ulysse <ulysse.rousselet@epitech.eu>
2024-04-18 00:35:00 +0200

    add: parsing section in json

--- src/ParseJson/ParseBody.hs
+++ src/ParseJson/ParseBody.hs
@@ -7,18 +7,8 @@
 
 module ParseJson.ParseBody (parseBody) where
 import Content (PContent(..), PParagraph(..), PParagraphType(..), PText(..), PTextType(..), PSection(..))
-import ParsingLib.AppendElemToDataStruct (addNewElemToContent)
 import ParsingLib.Lib (strToWordArray, strcmp, searchSymbol)
-
--- fonction pour récupérer le PContent selon le state (endroit)
--- fonction pour ajouter un PContent vide selon le state (endroit)
-
-addNewPContent :: [String] -> Bool -> PContent -> [PContent] -> [PContent]
-addNewPContent [] _ newC content = content ++ [newC]
-addNewPContent (x:xs) isBody newC content
-    | x == "section" && isBody == True = (init content) ++ [PSectionContent ( PSection {title = "", section_content = (addNewPContent (xs) True newC [(last content)])})]
-    | x == "section" && isBody == False = addNewPContent (xs) True newC content
-    | otherwise = addNewPContent (xs) True newC content
+import ParseJson.ParseFunction (notBracketChar, appendPContent, initPContent, lastPContent)
 
 -- Parsing paragraph
 
@@ -26,14 +16,15 @@ addParagraph :: String -> PContent -> PContent
 addParagraph str (PParagraphContent (PParagraph list)) = PParagraphContent (PParagraph (list ++ [PTextParagraph (PText [PString str])]))
 
 parseParagraph :: [String] -> [String] -> [PContent] -> Either String [PContent]
-parseParagraph state (x:xs) content = parseSymbol state xs False ((init content) ++ [addParagraph x (last content)])
+parseParagraph state (x:xs) content = parseSymbol state xs ((initPContent state content) ++ [addParagraph x (lastPContent state content)])
 
 -- check quel est le type du text et call la bonne fonction
 
 parseText :: [String] -> [String] -> [PContent] -> Either String [PContent]
 parseText _ [] _ = Left "Error: Missing ] in text"
 parseText state (x:xs) content
-    | last state == "paragraph" = parseParagraph state (x:xs) (addNewPContent state False (PParagraphContent (PParagraph [])) content)
+    | last state == "paragraph" = parseParagraph state (x:xs) content
+    | last state == "?" && x == "section" = parseSymbol ((init state) ++ ["section"]) xs content
 
 -- rempli la list d'état avec le type de contenu puis appelle parseText
 
@@ -41,17 +32,17 @@ parseSymbolParagraph :: [String] -> [String] -> [PContent] -> Either String [PCo
 parseSymbolParagraph _ [] _ = Left "Error: Missing ] in symbol"
 parseSymbolParagraph state ([]:xs) content = parseText state xs content
 parseSymbolParagraph state (x:xs) content
-    | head x == ']' && last state == "paragraph" = parseSymbol (init state) (tail x:xs) False content
+    | head x == ']' && last state == "paragraph" = parseSymbol (init state) (tail x:xs) content
     | otherwise = Right content
 
-parseSymbol :: [String] -> [String] -> Bool -> [PContent] -> Either String [PContent]
-parseSymbol _ [] _ content = Right content
-parseSymbol state ([]:xs) _ content = parseText state xs content
-parseSymbol state (x:xs) isComma content
-    | head x == ':' = parseSymbol state (tail x:xs) True content
-    | head x == '[' && isComma && last state == "section" = parseSymbol (state ++ ["content"]) (tail x:xs) False content
-    | head x == '[' && last state == "content" = parseSymbol (state ++ ["paragraph"]) (tail x:xs) False content
-    | head x == ' ' || head x == '\n' || head x == ',' = parseSymbol state (tail x:xs) isComma content
+parseSymbol :: [String] -> [String] -> [PContent] -> Either String [PContent]
+parseSymbol _ [] content = Right content
+parseSymbol state ([]:xs) content = parseText state xs content
+parseSymbol state (x:xs) content
+    | head x == '[' && last state == "section" = parseSymbol (state ++ ["content"]) (tail x:xs) content
+    | head x == '[' && last state == "content" = parseSymbol (state ++ ["paragraph"]) (tail x:xs) (appendPContent state (PParagraphContent (PParagraph [])) content)
+    | head x == '{' && last state == "content" = parseSymbol (state ++ ["?"]) (tail x:xs) content
+    | notBracketChar x = parseSymbol state (tail x:xs) content
     | otherwise = parseSymbolParagraph state (x:xs) content
 
 -- Parsing de la base
@@ -69,7 +60,7 @@ parseBaseLoop :: [String] -> [String] -> [PContent] -> Either String [PContent]
 parseBaseLoop _ [] content = Right content
 parseBaseLoop dataParsing (x:xs) content 
     | strcmp "header" x = parseHeader dataParsing xs content
-    | strcmp "body" x = parseSymbol (initDataParsing dataParsing) xs False content
+    | strcmp "body" x = parseSymbol (initDataParsing dataParsing) xs content
     | otherwise = Right content
 
 enterInSection :: [String] -> [String] -> [PContent] -> Either String [PContent]
