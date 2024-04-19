1d44b0f8153c6dd16c34ce78d025492d1641fc97
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-04-19 15:17:12 +0200

    mod: function to detect a symbol in a formatting text has been changed in markdown format to be more generic

--- src/ParseMarkdown/FormatText/DetectSymbol.hs
+++ src/ParseMarkdown/FormatText/DetectSymbol.hs
@@ -14,35 +14,27 @@ import ParseMarkdown.FormatText.TextType.Code (symbolCodedAlreadyOpen, symbolCod
 import ParseMarkdown.FormatText.TryAddBasicList (parseBasicChar)
 import ParsingLib.Lib (parseString)
 
+checkBold :: String -> DataText -> IO (String, DataText)
+checkBold str dataText
+    | isInBold dataText = symbolBoldAlreadyOpen str dataText
+    | otherwise = symbolBoldNotOpen str dataText
+
+checkItalic :: String -> DataText -> IO (String, DataText)
+checkItalic str dataText
+    | isInItalic dataText = symbolItalicAlreadyOpen str dataText
+    | otherwise = symbolItalicNotOpen str dataText
+
+checkCode :: String -> DataText -> IO (String, DataText)
+checkCode str dataText
+    | isInCode dataText = symbolCodedAlreadyOpen str dataText
+    | otherwise = symbolCodeNotOpen str dataText
+
 detectSymbol :: String -> DataText -> IO (String, DataText)
 detectSymbol str dataText
-    | Just (_, rightPart) <- isBold, not (isInItalic dataText) = do
-        if isInBold dataText
-            then
-                (symbolBoldAlreadyOpen rightPart dataText) 
-        else
-            (symbolBoldNotOpen rightPart dataText)
-
-    | Just (_, rightPart) <- isItalic = do
-        if isInItalic dataText
-            then
-                (symbolItalicAlreadyOpen rightPart dataText) 
-        else
-            (symbolItalicNotOpen rightPart dataText)
-    | Just (_, rightPart) <- isBold = do
-        if isInBold dataText
-            then
-                (symbolBoldAlreadyOpen rightPart dataText) 
-        else
-            (symbolBoldNotOpen rightPart dataText)
-
-    | Just (_, rightPart) <- isCode = do
-        if isInCode dataText
-            then
-                (symbolCodedAlreadyOpen rightPart dataText) 
-        else
-            (symbolCodeNotOpen rightPart dataText)
-
+    | Just (_, rightPart) <- isBold, not (isInItalic dataText) = checkBold rightPart dataText
+    | Just (_, rightPart) <- isItalic = checkItalic rightPart dataText
+    | Just (_, rightPart) <- isBold = checkBold rightPart dataText
+    | Just (_, rightPart) <- isCode = checkCode rightPart dataText
     | otherwise = do
         if (length str > 0)
             then do
